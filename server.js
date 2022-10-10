724390572b06d785ac35d4aea3bc8ab3bfce16a4
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2022-10-09 21:18:10 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongodb = require("./database/connect");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app
+  .use(cors())
+  .use(express.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT);
+    console.log(
+      "\x1b[34m%s\x1b[0m",
+      `Connected to DB and listening on ${PORT}`
+    );
+  }
+});
