882aba412cb0410b47848a11bfe573167fc0d2ed
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2022-10-09 23:59:33 -0600

    added swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/", require("./swagger.js"));
 router.use("/books", require("./books"));
 
 module.exports = router;
