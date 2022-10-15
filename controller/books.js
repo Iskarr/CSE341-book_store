551399556f41b45780fa68f2f10716fedf03b1b1
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2022-10-14 21:12:29 -0600

    added error handling

--- controller/books.js
+++ controller/books.js
@@ -14,19 +14,26 @@ const createBook = async (req, res) => {
       BookGenre: req.body.BookGenre,
     };
 
+    if (book.bookTitle == null || book.bookTitle == "") {
+      return res.status(404).json({
+        success: false,
+        error: "You must provide a book title",
+      });
+    }
+
     const response = await mongodb
       .getDb()
       .db()
       .collection("books")
       .insertOne(book);
     if (response.acknowledged) {
-      res.status(201).json(response);
+      res
+        .status(201)
+        .json({ success: true, id: book._id, message: "Book created!" });
     } else {
       res
         .status(500)
-        .json(
-          response.error || "Some error occurred while creating the contact."
-        );
+        .json(response.error || "Some error occurred while creating the book.");
     }
   } catch (error) {
     res.status(500).json(error);
@@ -48,14 +55,20 @@ const updateBook = async (req, res) => {
       BookGenre: req.body.BookGenre,
     };
 
+    if (!book) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
     const response = await mongodb
       .getDb()
       .db()
       .collection("books")
       .replaceOne({ _id: userId }, book);
     console.log(response);
-    if (response.modifiedCount > 0) {
-      res.status(204).send(response);
+    if (response.modifiedCount >= 0) {
+      res
+        .status(204)
+        .send({ success: true, id: book._id, message: "Book updated!" });
     } else {
       res
         .status(500)
@@ -72,6 +85,7 @@ const updateBook = async (req, res) => {
 const deleteBook = async (req, res) => {
   try {
     const userId = new ObjectId(req.params.id);
+
     const response = await mongodb
       .getDb()
       .db()
@@ -79,7 +93,7 @@ const deleteBook = async (req, res) => {
       .deleteOne({ _id: userId }, true);
     console.log(response);
     if (response.deletedCount > 0) {
-      res.status(200).send();
+      res.status(200).send({ message: `${userId} Successfully Deleted!` });
     } else {
       res
         .status(500)
@@ -96,7 +110,12 @@ const deleteBook = async (req, res) => {
 const getAllBooks = async (req, res) => {
   try {
     const result = await mongodb.getDb().db().collection("books").find();
-    result.toArray().then((lists) => {
+    result.toArray().then((err, lists) => {
+      if (err) {
+        res.status(400).json({
+          message: err,
+        });
+      }
       res.setHeader("Content-Type", "application/json");
       res.status(200).json(lists);
     });
@@ -114,7 +133,12 @@ const getSingleBook = async (req, res) => {
       .db()
       .collection("books")
       .find({ _id: userId });
-    result.toArray().then((lists) => {
+    result.toArray().then((err, lists) => {
+      if (err) {
+        res.status(400).json({
+          message: err,
+        });
+      }
       res.setHeader("Content-Type", "application/json");
       res.status(200).json(lists[0]);
     });
